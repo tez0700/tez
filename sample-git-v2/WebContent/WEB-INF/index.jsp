c1d7013abcd5b5821ce73dd4168af3d94a38a8c8
pc32 <pc32@naver.com>
2019-05-08 12:31:13 +0900

    index.jsp ,MemberVo.java 생

---
+++ sample-git-v2/WebContent/WEB-INF/index.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+
+<!DOCTYPE html>
+<html lang="ko">
+  <head>
+	 <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Spring Framework 열심히!!!">
+    <meta name="author" content="방권호">
+    <title>넥스트 IT 교육(Spring MVC)</title>
+  </head>
+
+
+  
+  	<body>
+  <!-- A JSP Template for Spring Framework -->
+  
+  <li>
+  인덱스 1
+  </li>
+  
+  
+      
+  </body>
+</html>
+    
---
+++ sample-git-v2/src/member/MemberVo.java
@@ -0,0 +1,42 @@
+package member;
+
+public class MemberVo {
+
+	
+	private String memId;
+	private String memName;
+	private String memPw;
+	private String memPhone;
+	private String memEmail;
+	public String getMemId() {
+		return memId;
+	}
+	public void setMemId(String memId) {
+		this.memId = memId;
+	}
+	public String getMemName() {
+		return memName;
+	}
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	public String getMemPw() {
+		return memPw;
+	}
+	public void setMemPw(String memPw) {
+		this.memPw = memPw;
+	}
+	public String getMemPhone() {
+		return memPhone;
+	}
+	public void setMemPhone(String memPhone) {
+		this.memPhone = memPhone;
+	}
+	public String getMemEmail() {
+		return memEmail;
+	}
+	public void setMemEmail(String memEmail) {
+		this.memEmail = memEmail;
+	}
+	
+}
